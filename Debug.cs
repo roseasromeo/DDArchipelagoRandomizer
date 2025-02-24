56a8557c2344cac57d26a2146fa606cd597d56f6
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2025-02-23 23:28:58 -0800

    add Debug utility class

---
+++ Debug.cs
@@ -0,0 +1,19 @@
+﻿namespace ArchipelagoRandomizer;
+
+static class Debug
+{
+	public static void Log(string message)
+	{
+		Plugin.Logger.LogInfo(message);
+	}
+
+	public static void LogWarning(string message)
+	{
+		Plugin.Logger.LogWarning(message);
+	}
+
+	public static void LogError(string message)
+	{
+		Plugin.Logger.LogError(message);
+	}
+}
