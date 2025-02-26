fa3279462cebf2cb7a737155ca46594bc1f195b2
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2025-02-25 16:31:24 -0800

    Use ModList's InitStatus property

--- Plugin.cs
+++ Plugin.cs
@@ -4,21 +4,33 @@ using AGM = DDoor.AlternativeGameModes;
 
 namespace ArchipelagoRandomizer;
 
-[BepInPlugin("deathsdoor.archipelagorandomizer", "ArchipelagoRandomizer", "1.0.0.0")]
+[BepInPlugin("deathsdoor.archipelagorandomizer", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency("deathsdoor.itemchanger"), BepInDependency("deathsdoor.alternativegamemodes")]
 public class Plugin : BaseUnityPlugin
 {
 	internal static new ManualLogSource Logger;
 
+	public int InitStatus { get; internal set; } = 0;
+
 	private void Awake()
 	{
-		// Plugin startup logic
-		Logger = base.Logger;
-		Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-
-		AGM.AlternativeGameModes.Add("Archipelago Rando", () =>
+		try
 		{
-			new ItemRandomizer();
-		});
+			// Plugin startup logic
+			Logger = base.Logger;
+			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+			AGM.AlternativeGameModes.Add("Archipelago", () =>
+			{
+				new ItemRandomizer();
+			});
+
+			InitStatus = 1;
+		}
+		catch (System.Exception err)
+		{
+			InitStatus = 2;
+			throw err;
+		}
 	}
 }
