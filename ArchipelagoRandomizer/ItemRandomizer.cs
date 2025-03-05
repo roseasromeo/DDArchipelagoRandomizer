ab101a71a106317fb79da8890e7ba7901fa0019b
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2025-03-05 09:36:12 -0800

    Add custom Item struct to handle sending items

--- ArchipelagoRandomizer/ItemRandomizer.cs
+++ ArchipelagoRandomizer/ItemRandomizer.cs
@@ -52,17 +52,36 @@ internal class ItemRandomizer : MonoBehaviour
 		item?.Trigger();
 	}
 
+	private void PickedUpItem(DDItem item)
+	{
+		Logger.Log($"Picked up {item.DisplayName} at {item.Location}!");
+	}
+
 	private void PlaceItems()
 	{
+		// Get ItemPlacements from scouted placements
 		List<ItemPlacement> itemPlacements = Archipelago.Instance.ScoutedPlacements.Select(kvp => new ItemPlacement(kvp.Value, kvp.Key)).ToList();
 		IC.SaveData data = IC.SaveData.Open();
 
 		foreach (ItemPlacement itemPlacement in itemPlacements)
 		{
-			data.Place(itemPlacement.Item, itemPlacement.Location);
+			// Get predefined item to use its icon
+			IC.Predefined.TryGetItem(itemPlacement.Item, out IC.Item predefinedItem);
+
+			// Don't use predefined icon for weapons because it errors at this point, since player doesn't exist yet,
+			// so it doesn't have a way to reference WeaponSwitcher to determine upgrade level
+			string icon = (itemPlacement.Item is "Bomb" or "Fire" or "Hookshot")
+				? itemPlacement.Item
+				: predefinedItem?.Icon ?? "Unknown";
+
+			// Place item. Using custom item here so we can override the Trigger() method
+			// for knowing when an item was picked up to send it to server
+			IC.Item item = new DDItem(itemPlacement.Item, icon, itemPlacement.Location);
+			data.Place(item, itemPlacement.Location);
 			Logger.Log($"Placed {itemPlacement.Item} at {itemPlacement.Location}");
 		}
 
+		// Save, since ItemChanger doesn't do it for us due to when we run this method
 		TitleScreen.instance.saveMenu.saveSlots[TitleScreen.instance.index].saveFile.Save();
 	}
 
@@ -70,5 +89,24 @@ internal class ItemRandomizer : MonoBehaviour
 	{
 		public string Item { get; private set; } = item;
 		public string Location { get; private set; } = location;
+	}
+
+	private readonly struct DDItem : IC.Item
+	{
+		public string DisplayName { get; }
+		public string Icon { get; }
+		public string Location { get; }
+
+		public readonly void Trigger()
+		{
+			Instance.PickedUpItem(this);
+		}
+
+		public DDItem(string displayName, string icon, string location)
+		{
+			DisplayName = displayName;
+			Icon = icon;
+			Location = location;
+		}
 	}
 }
