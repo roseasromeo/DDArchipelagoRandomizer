96c3fa71c3931a923e744cb4a3491c009e6c0c93
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2025-08-12 08:32:35 -0500

    delay caching of background sprite so it doesn't happen before scene load

--- ArchipelagoRandomizer/UIManager.cs
+++ ArchipelagoRandomizer/UIManager.cs
@@ -1,5 +1,6 @@
 ﻿using DDoor.AddUIToOptionsMenu;
 using DDoor.ArchipelagoRandomizer.UI;
+using UnityEngine.SceneManagement;
 
 namespace DDoor.ArchipelagoRandomizer;
 
@@ -8,12 +9,21 @@ internal class UIManager
 	public static readonly UIManager instance = new();
 	private static readonly ConnectionMenu connectionMenu = new();
 	private static readonly NotificationPopup notificationHandler = new();
+	private static bool cachedBackgroundSprite = false;
 
 	public static UIManager Instance => instance;
 
 	private UIManager()
 	{
-		CustomUI.CacheBackgroundSprite();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode _)
+	{
+		if (!cachedBackgroundSprite && scene.name == "TitleScreen")
+		{
+			CustomUI.CacheBackgroundSprite();
+		}
 	}
 
 	public void ShowConnectionMenu()
