9912fddfb748a502f787223f7a7f2a9dfdf4ad5f
roseasromeo <11944660+roseasromeo@users.noreply.github.com>
2025-09-11 19:49:23 -0500

    disable ItemChanger respawn softlock prevention if ReturnToSpawn is installed

--- ArchipelagoRandomizer/Plugin.cs
+++ ArchipelagoRandomizer/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using System.Runtime.CompilerServices;
 using UnityEngine.Events;
 using AGM = DDoor.AlternativeGameModes;
 
@@ -11,15 +12,17 @@ namespace DDoor.ArchipelagoRandomizer;
 [BepInDependency("deathsdoor.alternativegamemodes")]
 [BepInDependency("deathsdoor.magicui")]
 [BepInDependency("deathsdoor.adduitooptionsmenu")]
+[BepInDependency("deathsdoor.returntospawn", BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BaseUnityPlugin
 {
 	internal static new ManualLogSource Logger;
 	private static Plugin instance;
+	private Harmony harmony;
 
 	public static Plugin Instance => instance;
 	public int InitStatus { get; internal set; } = 0;
 
-	#nullable enable
+#nullable enable
 	internal event UnityAction? OnUpdate;
 
 	private void Awake()
@@ -36,10 +39,18 @@ public class Plugin : BaseUnityPlugin
 				ArchipelagoRandomizerMod.Instance.OnFileCreated();
 			});
 
-			new Harmony("deathsdoor.archipelagorandomizer").PatchAll();
+			harmony = new Harmony("deathsdoor.archipelagorandomizer");
+			harmony.PatchAll();
 			UIManager.Instance.AddOptionsMenuItems();
 
+			if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("deathsdoor.returntospawn"))
+			{
+				DisableItemChangerSpawnOverride();
+			}
+
 			InitStatus = 1;
+
+
 		}
 		catch (System.Exception err)
 		{
@@ -51,6 +62,20 @@ public class Plugin : BaseUnityPlugin
 	private void Update()
 	{
 		OnUpdate?.Invoke();
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+	private static void DisableItemChangerSpawnOverride()
+	{
+		// If ReturnToSpawn is installed, disable ItemChanger's override of the DoorOverrider
+		Instance.harmony.Unpatch(AccessTools.Method(typeof(DoorOverrider), nameof(DoorOverrider.activate)), HarmonyPatchType.Prefix, "deathsdoor.itemchanger");
+	}
+
+	private static void DisableItemChangerShortcutDoorTriggerOverride()
+	{
+		// For Entrance Randomization, we have to wrap IC's ShortcutDoor Trigger Prefix with some additional code to prevent receiving checks when coming through a door
+		// Thus, we unpatch it here and then redo the patch in EntranceRandomizer.cs
+		Instance.harmony.Unpatch(AccessTools.Method(typeof(ShortcutDoor), nameof(ShortcutDoor.Trigger)), HarmonyPatchType.Prefix, "deathsdoor.itemchanger");
 	}
 }
 
